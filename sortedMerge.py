b5d890c09b0b5e7d523a4f471906796a6d91a8b3
Chiaki Mizuta <chiaki.mizuta@holition.com>
2020-09-30 11:51:51 +0100

    seems to be a working program

---
+++ sortedMerge.py
@@ -0,0 +1,39 @@
+
+def getMiddle(oneArrayLen: int) -> int:
+    return oneArrayLen / 2
+
+def sortedMerge(A: list, B: list) -> list:
+    if A == []:
+        return B
+    if B == []:
+        return A
+
+    for i in B:
+        low = 0
+        high = len(A) - 1
+        mid = 0
+        while low <= high:
+            if i == 12:
+                print("HI")
+            if low == high:
+                if A[high] >= i:
+                    A.insert(high, i)
+                else:
+                    A.insert(high + 1, i)    
+                break
+            mid = int((low + high) / 2)
+            if i > A[mid]:
+                low = mid + 1
+            elif i < A[mid]:
+                high = mid - 1
+            else:
+                A.insert(mid, i)
+                break
+    return A
+
+a = [1, 2, 6, 9, 13, 14, 20]
+b = [3, 6, 7, 12, 21, 30]
+a = [1, 1, 1, 1]
+b = [2]
+sorted = sortedMerge(a, b)
+print("hello")
